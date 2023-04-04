f944252e063b8dc77bcb970857849f610e160a23
konfle <konrad.flejter@gmail.com>
2023-04-04 18:57:50 +0200

    Updated routes in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.config import settings
+from backend.routers import user, auth
 
 app = FastAPI()
+
+origins = [
+    settings.CLIENT_ORIGIN,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, tags=['Auth'], prefix='/api/auth')
+app.include_router(user.router, tags=['Users'], prefix='/api/users')
 
 
 @app.get('/api/health')
