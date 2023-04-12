86dc5414e046b11343394bed0d4a325b3b97564a
konfle <konrad.flejter@gmail.com>
2023-04-12 23:05:33 +0200

    Added endpoint for update post.

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,7 +1,7 @@
 import uuid
 from .. import schemas, models
 from sqlalchemy.orm import Session
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
 from ..database import get_db
 from backend.oauth2 import require_user
 
@@ -30,3 +30,22 @@ def create_post(post: schemas.CreatePostSchema, db: Session = Depends(get_db),
     db.commit()
     db.refresh(new_post)
     return new_post
+
+
+# Update Post
+@router.put('/{id}', response_model=schemas.PostResponse)
+def update_post(id: str, post: schemas.UpdatePostSchema, db: Session = Depends(get_db),
+                user_id: str = Depends(require_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    updated_post = post_query.first()
+
+    if not updated_post:
+        raise HTTPException(status_code=status.HTTP_200_OK,
+                            detail=f'No post with this id: {id} found')
+    if updated_post.user_id != uuid.UUID(user_id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='You are not allowed to perform this action')
+    post.user_id = user_id
+    post_query.update(post.dict(exclude_unset=True), synchronize_session=False)
+    db.commit()
+    return updated_post
