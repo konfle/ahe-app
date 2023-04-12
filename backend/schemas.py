1932c5ea9fed80e73142ca001d9a8c56e5d1fb7d
konfle <konrad.flejter@gmail.com>
2023-04-12 17:47:13 +0200

    Updated schemas.

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import uuid
+from typing import Optional, List
+
 from pydantic import BaseModel, EmailStr, constr
 
 
@@ -28,3 +30,48 @@ class UserResponse(UserBaseSchema):
     id: uuid.UUID
     created_at: datetime
     updated_at: datetime
+
+
+class FilteredUserResponse(UserBaseSchema):
+    id: uuid.UUID
+
+
+class PostBaseSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: Optional[uuid.UUID] = None
+
+    class Config:
+        orm_mode = True
+
+
+class CreatePostSchema(PostBaseSchema):
+    pass
+
+
+class PostResponse(PostBaseSchema):
+    id: uuid.UUID
+    user: FilteredUserResponse
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdatePostSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: Optional[uuid.UUID] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+
+
+class ListPostResponse(BaseModel):
+    status: str
+    results: int
+    posts: List[PostResponse]
