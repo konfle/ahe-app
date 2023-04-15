8186f1a6600c518d7ab27c32f48f86b9b46adce6
konfle <konrad.flejter@gmail.com>
2023-04-15 14:51:32 +0200

    Added role verification in case of use deletion.

--- backend/oauth2.py
+++ backend/oauth2.py
@@ -14,12 +14,12 @@ from .models import User
 class Settings(BaseModel):
     authjwt_algorithm: str = settings.JWT_ALGORITHM
     authjwt_decode_algorithms: List[str] = [settings.JWT_ALGORITHM]
-    authjwt_token_location: set = {'cookies', 'headers'}
-    authjwt_access_cookie_key: str = 'access_token'
-    authjwt_refresh_cookie_key: str = 'refresh_token'
+    authjwt_token_location: set = {"cookies", "headers"}
+    authjwt_access_cookie_key: str = "access_token"
+    authjwt_refresh_cookie_key: str = "refresh_token"
     authjwt_cookie_csrf_protect: bool = False
-    authjwt_public_key: str = base64.b64decode(settings.JWT_PUBLIC_KEY).decode('utf-8')
-    authjwt_private_key: str = base64.b64decode(settings.JWT_PRIVATE_KEY).decode('utf-8')
+    authjwt_public_key: str = base64.b64decode(settings.JWT_PUBLIC_KEY).decode("utf-8")
+    authjwt_private_key: str = base64.b64decode(settings.JWT_PRIVATE_KEY).decode("utf-8")
 
 
 @AuthJWT.load_config
@@ -42,25 +42,25 @@ def require_user(db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
         user = db.query(models.User).filter(models.User.id == user_id).first()
 
         if not user:
-            raise UserNotFound('User no longer exist')
+            raise UserNotFound("User no longer exist")
 
         if not user.verified:
-            raise NotVerified('You are not verified')
+            raise NotVerified("You are not verified")
 
     except Exception as e:
         error = e.__class__.__name__
         print(error)
-        if error == 'MissingTokenError':
+        if error == "MissingTokenError":
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='You are not logged in')
-        if error == 'UserNotFound':
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not logged in")
+        if error == "UserNotFound":
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='User no longer exist')
-        if error == 'NotVerified':
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="User no longer exist")
+        if error == "NotVerified":
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='Please verify your account')
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Please verify your account")
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='Token is invalid or has expired')
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is invalid or has expired")
     return user_id
 
 
@@ -76,9 +76,3 @@ async def get_current_user(db: Session = Depends(get_db), authorize: AuthJWT = D
 
     except Exception:
         raise HTTPException(status_code=401, detail="Invalid token")
-
-
-async def is_admin(current_user: User = Depends(get_current_user)):
-    if current_user.role != "admin":
-        raise HTTPException(status_code=403, detail="User does not have admin role")
-    return current_user
--- backend/routers/user.py
+++ backend/routers/user.py
@@ -9,17 +9,23 @@ from ..models import User
 router = APIRouter()
 
 
-@router.get('/me', response_model=schemas.UserResponse)
+@router.get("/me", response_model=schemas.UserResponse)
 def get_user_info(db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_user)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     return user
 
 
 @router.delete("/{user_id}")
-def delete_user(user_id: uuid.UUID, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+def delete_user(user_id: uuid.UUID, db: Session = Depends(get_db),
+                current_user: User = Depends(oauth2.get_current_user)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
+
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
+
+    if user_id != current_user.id and current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="You do not have permission to delete this user")
+
     db.delete(user)
     db.commit()
     return {"message": "User deleted"}
